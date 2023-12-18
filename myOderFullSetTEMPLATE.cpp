ab7fb49ddd0d8fd08125835a39e9a04d26934898
Shakib Absar <sabsar42@gmail.com>
2023-12-19 03:35:56 +0600

    Add files via upload
    Done

---
+++ myOderFullSetTEMPLATE.cpp
@@ -0,0 +1,52 @@
+// Bismillah hir-rahman nir-raheem
+// Shakib Absar - (_rasbaris)
+
+#include<bits/stdc++.h>
+#include <tuple>
+using namespace std;
+#define   ll          long long
+#define   ld          long double
+#define   pb          push_back
+#define   ff          first
+#define   ss          second
+#define   tc()        ll t; cin>>t; while(t--)
+#define   endl        '\n'
+#define   debug(x)    cout << " >> " << #x << " : " << x << endl
+#define   yes         printf("Yes\n")
+#define   no          printf("No\n")
+#define   PI          acos(-1.0)
+#define   Case(csNo)  cout <<"Case "<<csNo++<<": ";
+#define   all(v)      (v).begin(),(v).end()
+#define   print(v)    for(auto &i:v)cout<<i;
+#define   _rasbaris_  ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+// cout << "Case "<<q<<": ";
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+template <typename T, typename U> T ceil(T x, U y) {return (x > 0 ? (x + y - 1) / y : x / y);}
+template <typename T, typename U> T floor(T x, U y) {return (x > 0 ? x / y : (x - y + 1) / y);}
+
+const ll M = 1e9;
+// 4 direction
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+int main()
+{
+    _rasbaris_
+
+    tc()
+    {
+
+
+    }
+
+    return 0;
+}
+
+
